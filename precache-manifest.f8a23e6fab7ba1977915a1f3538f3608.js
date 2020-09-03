12bb2de1d36d08e1c4e11da72a426916d950dfbf
Denys Bondarchuk <bondarchuk520@gmail.com>
2020-09-03 12:17:52 +0300

    Updates

---
+++ precache-manifest.f8a23e6fab7ba1977915a1f3538f3608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a69e1fd2036ea27920e238439777be",
+    "url": "/firebase-react-redux-crud/index.html"
+  },
+  {
+    "revision": "1ca68178e6903c3f674f",
+    "url": "/firebase-react-redux-crud/static/css/main.16e9f930.chunk.css"
+  },
+  {
+    "revision": "f0a88ce57736c50439b3",
+    "url": "/firebase-react-redux-crud/static/js/2.c3ecac91.chunk.js"
+  },
+  {
+    "revision": "cb59ff19cf2dee29ace8b51198822910",
+    "url": "/firebase-react-redux-crud/static/js/2.c3ecac91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca68178e6903c3f674f",
+    "url": "/firebase-react-redux-crud/static/js/main.ff184d87.chunk.js"
+  },
+  {
+    "revision": "1d1a6b48b1d980324cae",
+    "url": "/firebase-react-redux-crud/static/js/runtime-main.9b824b2e.js"
+  }
+]);
